8ea02a54f1e760de64f812152e695cce1570f248
Ghoulyanie <127665359+Ghoulyanie@users.noreply.github.com>
2023-09-26 15:38:14 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+
+
+let songs = [
+
+    "/audio/Ayesha_x_Righteous_Feeling_Righteously_Yummy (1).mp3",
+    "/audio/Pr1videnye.mp3"
+]
+
+const path = songs[Math.floor(Math.random()*songs.length)];
+var sound = new Audio();
+sound.src = path;
+
+function togglePlay() {
+    if (sound.paused) {
+        sound.play();
+    }
+    else {
+        sound.pause();
+    }
+}
